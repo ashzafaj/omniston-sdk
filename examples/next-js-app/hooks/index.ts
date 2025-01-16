9c5f7305d2d21c98a9b37e5b9a876ac8fd0ad8f9
ashzafaj <shzadah79@gmail.com>
2025-01-17 02:57:26 +0330

    Delete examples/next-js-app directory

--- examples/next-js-app/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-export { useOmniston } from "@ston-fi/omniston-sdk-react";
-export { useDebounce } from "use-debounce";
-
-export { useRfq } from "./useRfq";
-export { useAssets } from "./useAssets";
